bd00fdd1e155a735f41480b60fb0f57d057b2b8f
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-01 18:32:21 +0000

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,16 @@
+"use strict" // treat js code as newer version
+
+// alert(1 + 2) // its node js, it will run in browser
+
+// Datatypes :-
+
+//   Primitive
+// number = 2 to power 53
+// BigInt
+// string = ""
+// boolean = true/false
+// null = standalone value (object typeof)
+// undefined
+// symbol = unique
+
+// Object
