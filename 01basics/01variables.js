3c505565015fe707fc97ab385615f82a1f0b71c8
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-01 17:19:42 +0000

    var config

---
+++ 01basics/01variables.js
@@ -0,0 +1,10 @@
+const myName = "Shailesh";
+let myNumber = 82192809;
+var myCity = "Sunder Nagar";
+let myCountry;
+//var is not used much
+myNumber = 9805936521;
+
+console.log(myName);
+console.table([myName, myNumber, myCity, myCountry]);
+//Shortcut to display various details at a time.
