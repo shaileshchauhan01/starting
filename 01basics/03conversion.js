11c6dcac85f911bfbcc3a9b95d1d309d64af63f0
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-02 12:23:35 +0000

    conversion of datatypes

---
+++ 01basics/03conversion.js
@@ -0,0 +1,18 @@
+let marks = 90;
+console.log(typeof marks);
+ let valueInString = String(marks);
+ console.log(typeof valueInString);
+
+let name = "Shailesh";
+console.log(typeof name);
+ let valueInNumber = Number(name);
+ console.log(valueInNumber); 
+// NaN alpha to number is not possible
+
+let that = 0;
+let booleanThat = Boolean(that);
+console.log(booleanThat);
+// 1 => true; 0 => false
+// "" => false
+// "Shailesh" => true
+
