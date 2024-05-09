fc512de2b7a0c3674cfbfec323b879910a97e76e
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-09 07:11:36 +0000

    objects

---
+++ 02basics/03objects.js
@@ -0,0 +1,41 @@
+//singleout
+//containers : object.create
+
+// object literals
+
+let mySymbol = Symbol("symbol1");
+let mySym = Symbol("symbol2");
+
+
+const jsUser = {
+    name: "Shailesh",
+    age: "21",
+    "my email": "shailesh@ok.com",
+    location: "Punjab",
+    [mySymbol]: "sym1",
+    mySym: "sym2"
+}
+
+console.log(jsUser.name);
+// console.log(jsUser.my email); // error follow below syntax.
+console.log(jsUser["name"]);
+
+console.log(jsUser.mySym, typeof(jsUser.mySym));
+console.log(jsUser[mySymbol], typeof(jsUser[mySymbol]));
+
+jsUser.name = "anku";
+Object.freeze(jsUser); // frezze every data
+
+jsUser.name = "pKl";
+console.log(jsUser);
+
+jsUser.greeting = function (){
+    console.log("Hello user");
+}
+
+jsUser.greetingTwo = function (){
+    console.log(`Hello user ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
