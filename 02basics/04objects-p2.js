e3bcd895fc4c8497705b6172fe641970fddf2718
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-17 07:00:57 +0000

    de-structure, obj-p2

--- 02basics/04objects-p2.js
+++ 02basics/04objects-p2.js
@@ -1,64 +1,64 @@
-// // OBJECT CONSTRUCTOR
+// OBJECT CONSTRUCTOR
 
-// // const tUser = new Object() // both works same, below and this syntax
-// const tUser = {}
+// const tUser = new Object() // both works same, below and this syntax
+const tUser = {}
 
-// tUser.id = 12;
-// tUser.email = "o@g.com"
+tUser.id = 12;
+tUser.email = "o@g.com"
 
-// console.log(tUser);
+console.log(tUser);
 
-// const regUser = {
-//     id : 123,
-//     name : {
-//         fullname : {
-//             fisrtname : "Shailesh",
-//             secondname : "Chauhan"
-//         }
-//     }
-// }
+const regUser = {
+    id : 123,
+    name : {
+        fullname : {
+            fisrtname : "Shailesh",
+            secondname : "Chauhan"
+        }
+    }
+}
 
-// console.log(regUser.name);
-// console.log(regUser.name.allname?.fullname);
-
-
-// const obj1 = {1: "a", 2: "b"}
-// const obj2 = {3: "a", 4: "b"}
-
-// // const obj3 = {obj1,obj2} // not recommended
-
-// // const obj3 = Object.assign(obj1, obj2) // both syntax give result same 
-// // const obj3 = Object.assign({}, obj1, obj2)
-
-// const obj3 = {...obj1, ...obj2} // spread
-// console.log(obj3);
+console.log(regUser.name);
+console.log(regUser.name.allname?.fullname);
 
 
-// const users =[
-//     {
-//         id: 1,
-//         email: "o@gmail.com"
-//     },
-//     {
-//         id: 2,
-//         email: "p@gmail.com"
-//     },
-//     {
-//         id: 3,
-//         email: "q@gmail.com"
-//     }
-// ]
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
 
-// console.log(users[2].email); 
+// const obj3 = {obj1,obj2} // not recommended
+
+// const obj3 = Object.assign(obj1, obj2) // both syntax give result same 
+// const obj3 = Object.assign({}, obj1, obj2)
+
+const obj3 = {...obj1, ...obj2} // spread
+console.log(obj3);
 
 
-// console.log(tUser);
-// console.log(Object.keys(tUser));
-// console.log(Object.values(tUser));
-// console.log(Object.entries(tUser));
+const users =[
+    {
+        id: 1,
+        email: "o@gmail.com"
+    },
+    {
+        id: 2,
+        email: "p@gmail.com"
+    },
+    {
+        id: 3,
+        email: "q@gmail.com"
+    }
+]
 
-// console.log(tUser.hasOwnProperty("email"));
-// console.log(tUser.hasOwnProperty("name"));
+console.log(users[2].email); 
+
+
+console.log(tUser);
+console.log(Object.keys(tUser));
+console.log(Object.values(tUser));
+console.log(Object.entries(tUser));
+
+console.log(tUser.hasOwnProperty("email"));
+console.log(tUser.hasOwnProperty("name"));
 
 
 //DE-STRUCTURE
