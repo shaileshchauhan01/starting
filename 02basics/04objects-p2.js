63bae127da6bd77b9afd62a546342fe1535a49df
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-17 06:59:20 +0000

    de-structor, obj-p2

--- 02basics/04objects-p2.js
+++ 02basics/04objects-p2.js
@@ -1,63 +1,96 @@
-// OBJECT CONSTRUCTOR
+// // OBJECT CONSTRUCTOR
 
-// const tUser = new Object() // both works same, below and this syntax
-const tUser = {}
+// // const tUser = new Object() // both works same, below and this syntax
+// const tUser = {}
 
-tUser.id = 12;
-tUser.email = "o@g.com"
+// tUser.id = 12;
+// tUser.email = "o@g.com"
 
-console.log(tUser);
+// console.log(tUser);
 
-const regUser = {
-    id : 123,
-    name : {
-        fullname : {
-            fisrtname : "Shailesh",
-            secondname : "Chauhan"
-        }
-    }
+// const regUser = {
+//     id : 123,
+//     name : {
+//         fullname : {
+//             fisrtname : "Shailesh",
+//             secondname : "Chauhan"
+//         }
+//     }
+// }
+
+// console.log(regUser.name);
+// console.log(regUser.name.allname?.fullname);
+
+
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+
+// // const obj3 = {obj1,obj2} // not recommended
+
+// // const obj3 = Object.assign(obj1, obj2) // both syntax give result same 
+// // const obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2} // spread
+// console.log(obj3);
+
+
+// const users =[
+//     {
+//         id: 1,
+//         email: "o@gmail.com"
+//     },
+//     {
+//         id: 2,
+//         email: "p@gmail.com"
+//     },
+//     {
+//         id: 3,
+//         email: "q@gmail.com"
+//     }
+// ]
+
+// console.log(users[2].email); 
+
+
+// console.log(tUser);
+// console.log(Object.keys(tUser));
+// console.log(Object.values(tUser));
+// console.log(Object.entries(tUser));
+
+// console.log(tUser.hasOwnProperty("email"));
+// console.log(tUser.hasOwnProperty("name"));
+
+
+//DE-STRUCTURE
+
+let course = {
+    name : "JS code",
+    duration : "60 hours",
+    instructor : "Shailesh",
+    price : "free of cost"
 }
 
-console.log(regUser.name);
-console.log(regUser.name.allname?.fullname);
+// course.name // way to access but what if want to access different times
+
+const {name} = course // that's its is used
+const {duration: dur} = course // we can also chnage the name 
+console.log(name)
+console.log(dur)
 
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
+// API
+// api is in object/ JSON
+{
+    "name": "shailesh"
+    "age" : 21,
+    "last name": "chauhan"
+}
 
-// const obj3 = {obj1,obj2} // not recommended
-
-// const obj3 = Object.assign(obj1, obj2) // both syntax give result same 
-// const obj3 = Object.assign({}, obj1, obj2)
-
-const obj3 = {...obj1, ...obj2} // spread
-console.log(obj3);
-
-
-const users =[
-    {
-        id: 1,
-        email: "o@gmail.com"
-    },
-    {
-        id: 2,
-        email: "p@gmail.com"
-    },
-    {
-        id: 3,
-        email: "q@gmail.com"
-    }
+// api also in array
+[
+    {},
+    {}
 ]
 
-console.log(users[2].email); 
-
-
-console.log(tUser);
-console.log(Object.keys(tUser));
-console.log(Object.values(tUser));
-console.log(Object.entries(tUser));
-
-console.log(tUser.hasOwnProperty("email"));
-console.log(tUser.hasOwnProperty("name"));
-
-
+// randomuserme // free api 
+// JSON FORMATTER // api formatter
