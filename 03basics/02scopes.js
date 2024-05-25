e1c572f1e265c838507f44bc9a8597ba89937e33
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-25 12:32:38 +0000

    scopes

--- 03basics/02scopes.js
+++ 03basics/02scopes.js
@@ -1,24 +1,62 @@
-{}          // => Scopes
-            // checking scopes in console and node is Different
+// {}          // => Scopes
+//             // checking scopes in console and node is Different
 
-let a = 10;         // Block Scope 
-const b = 20;       // Block Scope
-var c = 30;         // Global Scope
+// let a = 10;         // Block Scope 
+// const b = 20;       // Block Scope
+// var c = 30;         // Global Scope
+
+// if(true){
+//     let a = 100;
+//     console.log(`Inner let: ${a}`)
+// }
+// console.log(`Outer let: ${a}`)
+
+// if(true){
+//     const b = 100;
+//     console.log(`Inner const: ${b}`)
+// }
+// console.log(`Outer const: ${b}`)
+
+// if(true){
+//     var c = 100;
+//     console.log(`Inner var: ${c}`)
+// }
+// console.log(`Outer var: ${c}`)
+
+
+function one(){
+    const username = "Shailesh"
+
+    function two(){
+        const  userage = 21;
+        let userwork = "study";
+        console.log(`${username} doing his ${userwork}`);
+    }
+    // console.log(userage);                            // non exexcutable bcz its ouside the function or block
+    two()
+}
+one()
+
 
 if(true){
-    let a = 100;
-    console.log(`Inner let: ${a}`)
-}
-console.log(`Outer let: ${a}`)
+    const username = "Shailesh";
 
-if(true){
-    const b = 100;
-    console.log(`Inner const: ${b}`)
+    if(username === "Shailesh"){
+        const usersite = " Stray care";
+        console.log(username + usersite)
+    }
+    // console.log(usersite)
 }
-console.log(`Outer const: ${b}`)
 
-if(true){
-    var c = 100;
-    console.log(`Inner var: ${c}`)
+
+console.log(addOne(4));             // if this written before function it will work
+function addOne(num){               // function
+    return num+1;
 }
-console.log(`Outer var: ${c}`)
+// addOne(4);
+
+// addTwo(4);                          // if this written before function it will not work
+let two = function addTwo(num){     // expression : when holds in variable (also function)
+    return num+1
+}
+// addTwo(4);
