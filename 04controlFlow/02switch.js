c62049015dc94bb9dc445ed95937c0fbef9a1c6c
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-27 11:28:40 +0000

    switch statement

---
+++ 04controlFlow/02switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//     case: value;
+//     break;
+
+//     default: value;
+// }
+
+//number
+const month = 3;
+
+switch(month){
+    case 1 : console.log("January");
+    break;
+    case 2 : console.log("February");
+    break;
+    case 3 : console.log("March");
+    break;
+    case 4 : console.log("April");
+    break;
+
+    default : console.log("not Included") 
+}
+
+//string
+const months = "mar";
+
+switch(months){
+    case "jan" : console.log("January");
+    break;
+    case "feb" : console.log("February");
+    break;
+    case "mar" : console.log("March");
+    break;
+    case "apr" : console.log("April");
+    break;
+
+    default : console.log("not Included") 
+}
