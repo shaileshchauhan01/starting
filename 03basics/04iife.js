a35852a35a5668e91d096a813aa8c4c11eb7561b
Shailesh Chauhan <114944171+shaileshchauhan01@users.noreply.github.com>
2024-05-27 05:11:57 +0000

    IIFE

--- 03basics/04iife.js
+++ 03basics/04iife.js
@@ -14,3 +14,5 @@
 ((name) => {
     console.log(`${name}`)
 })("Shailesh");
+
+// global scope ke pollution se problem hoti hai kyi baar, to jo global scope ke varibales hai ya jo bhi uskse declaration hai use htane ke liye IIFE ka use kiya jata hai
